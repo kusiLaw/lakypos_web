a72da6de02f37e46d58316c5e5218e4db1e5a390
kusi <lawcubegsm@gmail.com>
2023-06-18 15:08:45 +0000

    Add under-development notification

--- src/components/popwindow/popwindow.js
+++ src/components/popwindow/popwindow.js
@@ -8,7 +8,7 @@ const PopUpWindow = ({children , closeFn, display = false}) => {
   }
 
   return (
-     <div className='fixed flex gap-5 justify-center items-center w-full top-0 right-0 left-0 bottom-0 bg-black/50 z-50   '>
+     <div className='fixed flex gap-5 justify-center px-3 items-center w-full top-0 right-0 left-0 bottom-0 bg-black/50 z-50   '>
       
       <div className='bg-context_color h-fit py-2 px-2 md:py-5 md:px-5 text-white drop-shadow-2xl '>
          <div className='flex justify-end  text-2xl text-white font-light mb-5 '
---
+++ src/components/underdev/underdev.js
@@ -0,0 +1,27 @@
+import { useState, useContext } from "react";
+import PopUpWindow from "../popwindow/popwindow";
+import { NotifyMeContext } from '@/pages/provider';
+
+
+const UnderDev = () => {
+ const [display, setDisplay] = useState(true)
+ const {displayNotifyMe, setDisplayNotifyMe} =  useContext(NotifyMeContext)
+
+  return (
+    <PopUpWindow display={display} closeFn={setDisplay}>
+      <div className="px-2 md:px-8">
+        <h2 className="text-2xl font-medium text-center">Welcome to LakyPOS</h2>
+        <p className="py-8 text-lg">This page is under development. Some component may not 
+           work as expected. <br/> We are working very hard to make it become fully functional before 31-07-2023
+           
+           <br/>
+   
+        </p>
+
+        <p onClick={()=>{setDisplay(false); setDisplayNotifyMe(true)}} className="text-end text-default_blue cursor-pointer text-lg">Notify Me </p>
+      </div>
+    </PopUpWindow>
+  )
+}
+
+export default UnderDev
